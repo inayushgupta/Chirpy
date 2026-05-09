c6738cf9691789e1a1dc6c938add96a04239071e
Ayush Gupta <in.ayushgupta@gmail.com>
2026-05-09 07:04:24 +0530

    Basic file server ready

--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	handler := http.FileServer(http.Dir("."))
+
+	mux.Handle("/", handler)
+
 	var server http.Server
 	server.Handler = mux
 	server.Addr = ":8080"
-
 
 	fmt.Println("Starting Server @8080")
 	err := server.ListenAndServe()
